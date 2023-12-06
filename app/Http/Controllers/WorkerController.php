1797231e4f4d86cf7f372d039903093f69dc7ad4
Alex <connectionwithme1@gmail.com>
2023-12-06 16:26:42 +0200

    for v 6

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -2,16 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Worker;
 use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
-    public function index() {
-        return "This index action of Worker Controller";
+    public function index()
+    {
+        $workers = Worker::all();
+        foreach ($workers as $worker) {
+            dump($worker->name);
+        }
+
     }
 
     public function show() {
-        return "This show action of Worker Controller";
+        $worker = Worker::find(1);
+        dd($worker->toArray());
+    }
+
+    public function create()
+    {
+        $worker = [
+            'name' => 'Mark',
+            'surname' => 'Markov',
+            'email' => 'Markov@gmail.ru',
+            'age' => 20,
+            'description' => 'Im Ivan',
+            'is_married' => false,
+        ];
+        Worker::create($worker);
+
+
+        return "Ivan was created";
+
+    }
+
+    public function update()
+    {
+        $worker = Worker::find(2);
+        $worker->update([
+            'name' => 'Maksim',
+            'surname' => 'Maksimov',
+            'description' => 'My name is Maksim',
+        ]);
+    }
+
+    public function delete()
+    {
+        $worker = Worker::find(3);
+        $worker->delete();
+        return "Was deleted";
     }
 
 }
